9efed18f31214d1d28b0a72db1767ca98299a052
Nataliia4444 <127755653+Nataliia4444@users.noreply.github.com>
2024-05-08 00:21:29 +0200

    add basket default page

--- src/pages/Basket/Basket.jsx
+++ src/pages/Basket/Basket.jsx
@@ -1,24 +1,35 @@
+import { useState } from "react";
 import CardProduct from "../../components/CardProduct/CardProduct";
 import css from "./basket.module.css";
-function Basket({ basketData }) {
-  //   console.log(ListId);
 
+function Basket({ basketData }) {
+  const [basket, setBasket] = useState(false);
+  console.log(basketData);
+  if (basketData.lenght > 1) {
+    setBasket(true);
+  }
   return (
     <div className={css.container}>
       <h2>BASKET</h2>
-      <ul className={css.productList}>
-        {basketData.map(({ price, id, title, description }) => {
-          return (
-            <CardProduct
-              price={price}
-              description={description}
-              key={id}
-              title={title}
-              id={id}
-            />
-          );
-        })}
-      </ul>
+      {basket ? (
+        <ul className={css.productList}>
+          {basketData.map(({ price, id, title, description }) => {
+            return (
+              <CardProduct
+                price={price}
+                description={description}
+                key={id}
+                title={title}
+                id={id}
+              />
+            );
+          })}
+        </ul>
+      ) : (
+        <div className={css.basketDefault}>
+          <h1 className={css.titleDefault}> The basket is empty</h1>
+        </div>
+      )}
     </div>
   );
 }
